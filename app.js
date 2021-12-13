feb3ddb3e55b9072e8f5c25274010295645727a3
Katrina Apel <kapel2810@gmail.com>
2021-12-13 08:48:41 +0200

    ex 2 done

--- app.js
+++ app.js
@@ -1,55 +1,113 @@
 
+/////////////////////////////
+//------------ex1--------
+// const showAllData = async () => {
+//     let usersList = await getAllUsersFomServer()
+//     displayAllUsersInTable(usersList)
+// }
+// const getAllUsersFomServer = () => {
+//     return new Promise((resolve, reject) => {
+//         const ajax = new XMLHttpRequest()
+
+//         ajax.onreadystatechange = () => {
+
+//             if (ajax.readyState === 4) {
+//                 if (ajax.status === 200) {
+//                     let usersListFromJason = JSON.parse(ajax.responseText)
+//                     // console.log(usersList)
+//                     resolve(usersListFromJason)
+//                 }else {
+//                     reject('Status is not ok')
+//                 }
+//             }
+//         }
+
+//         ajax.open('GET', 'https://jsonplaceholder.typicode.com/users')
+
+//         ajax.send()
+
+//     })
+
+// }
+// const displayAllUsersInTable = (usersList) => {
+//     let tableBodyElement = document.getElementById('table-body')
+//     let table = ''
+//     for (let userElement of usersList) {
+//         //console.log(userElement)
+
+//         table += `
+//                                 <tr>
+//                                     <td>${userElement.name}</td>
+//                                     <td>${userElement.username}</td>
+//                                     <td>${userElement.email}</td>
+//                                     <td>${userElement.phone}</td>
+//                                     <td>${userElement.address.city}</td>
+//                                     <td>${userElement.address.street}</td>
+//                                     <td>${userElement.address.zipcode}</td>
+//                                     <td>${userElement.company.name}</td>
+//                                 </tr>
+
+//                     `
+//     }
+//     tableBodyElement.innerHTML = table
+
+// }
+
+
+/////////////////////////////////////
+//------ex 2------
+
+let inputElement = document.getElementById('input-id')
+let displayElement = document.getElementById('display')
 
 const showAllData = async () => {
     let usersList = await getAllUsersFomServer()
-    displayAllUsersInTable(usersList)
-}
+    displayUsersInList(usersList)
 
+}
 
 const getAllUsersFomServer = () => {
     return new Promise((resolve, reject) => {
-        const ajax = new XMLHttpRequest()
+        let ajax = new XMLHttpRequest()
 
         ajax.onreadystatechange = () => {
-
             if (ajax.readyState === 4) {
                 if (ajax.status === 200) {
-                    let usersListFromJason = JSON.parse(ajax.responseText)
-                    // console.log(usersList)
-                    resolve(usersListFromJason)
+                    let usersList = JSON.parse(ajax.responseText)
+                    resolve(usersList)
                 }else {
-                    reject('Status is not ok')
+                    reject()
                 }
             }
         }
-
         ajax.open('GET', 'https://jsonplaceholder.typicode.com/users')
-
         ajax.send()
 
     })
-
 }
-const displayAllUsersInTable = (usersList) => {
-    let tableBodyElement = document.getElementById('table-body')
-    let table = ''
+const displayUsersInList = (usersList) => {
+    
+    let list = ''
     for (let userElement of usersList) {
-        //console.log(userElement)
+        if (inputElement.value >= 1 && inputElement.value <= 10) {
 
-        table += `
-                                <tr>
-                                    <td>${userElement.name}</td>
-                                    <td>${userElement.username}</td>
-                                    <td>${userElement.email}</td>
-                                    <td>${userElement.phone}</td>
-                                    <td>${userElement.address.city}</td>
-                                    <td>${userElement.address.street}</td>
-                                    <td>${userElement.address.zipcode}</td>
-                                    <td>${userElement.company.name}</td>
-                                </tr>
-                    
-                    `
+            list += `
+                        
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item"> <b>name</b>: ${userElement.name} 
+                                    <b>username</b>: ${userElement.username}  
+                                    <b>phone number </b>: ${userElement.email} 
+                                    <b>city</b>: ${userElement.address.city} 
+                                    <b>street</b>: ${userElement.address.street} 
+                                    <b>zipcode</b>: ${userElement.address.zipcode} 
+                                    <b>company</b>: ${userElement.company.name}</li>
+                                </ul>
+                        `
+        }else {
+            // alert( 'the number is out of range')
+        }
+           displayElement.innerHTML = list
+          
     }
-    tableBodyElement.innerHTML = table
-
+    
 }
